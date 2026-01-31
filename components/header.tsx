b7b19a9f78b669545c5e4ffb64d3ae9a79e545c8
OUSSAMA22-cy <oussamacrypto4@gmail.com>
2026-01-31 22:55:42 +0100

    first commit

--- components/header.tsx
+++ components/header.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
-
+'use client'
+import Link from 'next/link'
+import Image from 'next/image'
+import { usePathname } from 'next/navigation'
+import { cn } from "@/lib/utils"
 export default function Header() {
+    const path = usePathname();
     return (
-        <div>
+        <header>
+            <div className="main-container">
+                <div className="inner">
+                    <Link href="/">
+                        <Image src="/logo.png" alt="Logo" width={132} height={40} />
+                    </Link>
+                    <nav>
+                        <Link href="/" className={cn('nav-link', { 'is-active': path === '/', 'is-home': true })}>Home</Link>
+                        <p>search</p>
+                        <Link href="/coins" className={cn('nav-link', { 'is-active': path === '/coins' })}>Coins</Link>
+                        <Link href="/news" className={cn('nav-link', { 'is-active': path === '/news' })}>News</Link>
+                        <Link href="/portfolio" className={cn('nav-link', { 'is-active': path === '/portfolio' })}>Portfolio</Link>
+                        <Link href="/settings" className={cn('nav-link', { 'is-active': path === '/settings' })}>Settings</Link>
 
-        </div>
+                    </nav>
+                </div>
+            </div>
+        </header>
+
+
     )
 }
